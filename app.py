23b303970d447600e42b428c0bd25d988b8cf559
Andrii Terets <terets.andrey@gmail.com>
2022-06-27 20:18:13 +0300

    first verion

---
+++ app.py
@@ -0,0 +1,75 @@
+import queue
+import sounddevice as sd
+import vosk
+import json
+import words
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from skills import *
+
+q = queue.Queue()
+
+model = vosk.Model('vosk_model')
+
+# Выбор микрофона и динамиков (сейчас по умолчанию)
+device = sd.default.device
+
+#Получение частоты микрофона
+samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
+
+def recognize(data, vectorizer, clf):
+    '''Анализ распознаной речи'''
+
+    # Произнесено ли имя бота
+    trg = words.TRIGGERS.intersection(data.split())
+    if not trg:
+        return
+
+    # Перевод полученого текста в вектор, сравнение с вариантами
+    data.replace(list(trg)[0], '')
+    text_vector = vectorizer.transform([data]).toarray()[0]
+    answer = clf.predict([text_vector])[0]
+
+    # Получение имени функции с data_set
+    func_name = answer.split()[0]
+
+    #Озвучка
+    speaker(answer.replace(func_name, ''))
+    exec(func_name + '()')
+
+def callback(indata):
+    '''Добавляет в очередь семплы из потока.
+    вызывается каждый раз при наполнении blocksize
+    в sd.RawInputStream'''
+    q.put(bytes(indata))
+
+
+def main():
+    '''
+        Обучаем матрицу ИИ
+        и постоянно слушаем микрофон
+    '''
+
+    #Обучение матрицы на data_set модели
+    vectorizer = CountVectorizer()
+    vectors = vectorizer.fit_transform(list(words.data_set.keys()))
+
+    clf = LogisticRegression()
+    clf.fit(vectors, list(words.data_set.values()))
+
+    del words.data_set
+
+    # постоянная прослушка микрофона
+    with sd.RawInputStream(samplerate=samplerate, blocksize = 16000, device=device[0], dtype='int16',
+                                channels=1, callback=callback):
+
+        rec = vosk.KaldiRecognizer(model, samplerate)
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data):
+                data = json.loads(rec.Result())['text']
+                recognize(data, vectorizer, clf)
+
+
+if __name__ == '__main__':
+    main()
---
+++ skills.py
@@ -0,0 +1,62 @@
+import os, webbrowser, sys, requests, subprocess, pyttsx3
+
+engine = pyttsx3.init()
+# Скорость речи
+engine.setProperty('rate', 180)
+
+
+def speaker(text):
+    """Активация речи"""
+    engine.say(text)
+    engine.runAndWait()
+
+
+def passive():
+    pass
+
+
+def game():
+    """Открывает фортнайт и закрывает хром"""
+    subprocess.Popen('Q:/Fortnite/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe')
+    os.system("TASKKILL /F /IM chrome.exe")
+
+
+def game_with_friends():
+    """Открывает фортнайт, дискорд, закрывает браузер"""
+    subprocess.Popen('Q:/Fortnite/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe')
+    subprocess.Popen("C:/Users/teret/AppData/Local/Discord/app-1.0.9005/Discord.exe")
+    os.system("TASKKILL /F /IM chrome.exe")
+
+
+def weather():
+    '''Берёт данные о погоде с сайта openweather '''
+    try:
+        params = {'q': 'Kyiv', 'units': 'metric', 'lang': 'ru', 'appid': 'api-token'}
+        response = requests.get(f'https://api.openweathermap.org/data/2.5/weather', params=params)
+        if not response:
+            raise
+        w = response.json()
+        speaker(f"На улице {w['weather'][0]['description']} {round(w['main']['temp'])} градусов")
+
+    except:
+        speaker('Произошла ошибка при попытке запроса к ресурсу API, проверь код')
+
+
+def reload_pc():
+    '''Перезагрузка пк'''
+    os.system('shutdown /r')
+
+
+def off_bot():
+    '''Остановка работы программы'''
+    sys.exit()
+
+
+def off_pc():
+    '''Выключение компьютера'''
+    os.system('shutdown /s')
+
+
+def browser():
+    '''Открыть браузер'''
+    webbrowser.open('https://www.youtube.com/', new=2)
